fb0093e34103e068cbf172130b09ee0f6c29b5f4
brendansailer <57545584+brendansailer@users.noreply.github.com>
2021-04-13 19:16:54 -0400

    Optional changes to get familiar with pandas/numpy

---
+++ README.md
@@ -0,0 +1,7 @@
+Virtual Environment Setup:
+- Upgrade pip: python3 -m pip install --user --upgrade pip
+- Install virtualenv: python3 -m pip install --user virtualenv
+- Create the venv: python3 -m venv env
+- Activate the venv: source env/bin/activate
+- Install requirements: pip install -r requirements.txt
+- Exit: deactivate
--- dataCollection.py
+++ dataCollection.py
@@ -5,6 +5,7 @@ from cfbd.rest import ApiException
 from pprint import pprint
 import json
 import pandas as pd
+import numpy as np
 import csv
 import requests
 
@@ -16,9 +17,14 @@ years = list(range(2000, 2021))
 
 weeks = list(range(0,16))
 
+# Test on reduced dataset
+#years = list(range(2020, 2021))
+#weeks = list(range(0,5))
+
 req = []
 df = pd.DataFrame()
 for year in years:
+    #print("Working on year: " + str(year))
     for week in weeks:
 
         parameters = {"year":year, "week":week, "team": "Notre Dame"}
@@ -31,10 +37,14 @@ for year in years:
         continue
 
 df_trunc = df[["season", "week", "home_team", "home_points", "away_team", "away_points"]]
+# If we wanted to add a column of the winner (Syntax - np.where(condition, value if condition is true, value if condition is false))
+# I haven't used pandas before and we get a weird error, so we might need to try something else.  I think it works though since we never drop the home/away team name
+df_trunc["winner"] = np.where(df_trunc["home_points"] > df_trunc["away_points"], df_trunc["home_team"], df_trunc["away_team"])
 
 df2 = pd.DataFrame()
 team = "Notre Dame"
 for year in years:
+    #print("Working on year: " + str(year))
     parameters = {"year":year, "team":team}
 
     req = requests.get(url2, params = parameters, headers=headers)
@@ -45,6 +55,8 @@ for year in years:
     continue
 
 df2_trunc = df2[["first_name", "last_name"]]
+# I think this could be good to display the coach's name in order with a space
+df2_trunc["coach"] = df2_trunc["first_name"] + " " + df2_trunc["last_name"]
 df2_trunc["year"] = years
 
 print(df_trunc)
@@ -53,3 +65,4 @@ print(df2_trunc)
 df_merge = pd.merge(df_trunc, df2_trunc, left_on='season', right_on='year', how='left')
 df_merge.drop("year", inplace=True, axis=1)
 df_merge.to_csv('data.csv', sep=',', encoding='utf-8')
+#df_merge.to_csv('data_test.csv', sep=',', encoding='utf-8')
